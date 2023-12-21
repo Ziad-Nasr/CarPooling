a02927c2f44468f0f7a81902c202665a82392dd1
ZiadNasr <ziadnasr988@gmail.com>
2023-12-21 22:54:20 +0200

    SQFlite is finished, used to display the profile

---
+++ lib/components/database.dart
@@ -0,0 +1,86 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+class DatabaseHelper {
+  static final _databaseName = "ProfileDatabase.db";
+  static final _databaseVersion = 2;
+  static final table = 'profile';
+
+  static final columnEmail = 'email';
+  static final columnName = 'name';
+  static final columnLevel = 'level';
+  static final columnPhoneNumber = 'phoneNumber';
+  DatabaseHelper() {}
+  // Make this a singleton class.
+  DatabaseHelper._privateConstructor();
+  static final DatabaseHelper instance = DatabaseHelper._privateConstructor();
+
+  // Only have a single app-wide reference to the database.
+  static Database? _database;
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    // Lazily instantiate the db the first time it is accessed.
+    _database = await _initializeDatabase();
+    return _database!;
+  }
+
+  // Open the database and create the table.
+  _initializeDatabase() async {
+    String documentsDirectory = await getDatabasesPath();
+    String path = join(documentsDirectory + _databaseName);
+    return await openDatabase(path,
+        version: _databaseVersion, onCreate: _onCreate);
+  }
+
+  // SQL code to create the database table.
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+          CREATE TABLE $table (
+            $columnEmail TEXT PRIMARY KEY,
+            $columnName TEXT NOT NULL,
+            $columnLevel TEXT NOT NULL,
+            $columnPhoneNumber TEXT NOT NULL
+          )
+          ''');
+  }
+
+  // Helper methods to insert, query, update, and delete:
+
+  // Insert a profile row.
+  Future<int> insert(Map<String, dynamic> row) async {
+    Database db = await instance.database;
+    return await db.insert(table, row);
+  }
+
+  Future<List<Map<String, dynamic>>> queryRows(String email) async {
+    Database db = await instance.database;
+    return await db.query(table, where: '$columnEmail = ?', whereArgs: [email]);
+  }
+
+  // Query all rows.
+  reading(sql) async {
+    Database db = await instance.database;
+    return await db.rawQuery(sql);
+  }
+
+  // Update a profile row.
+  updating(sql) async {
+    Database db = await instance.database;
+    return await db.rawUpdate(sql);
+  }
+
+  // Future<int> update(Map<String, dynamic> row) async {
+  //   Database db = await instance.database;
+  //   String email = row[columnEmail];
+  //   return await db
+  //       .update(table, row, where: '$columnEmail = ?', whereArgs: [email]);
+  // }
+
+  // Delete a profile row.
+  Future<int> delete(String email) async {
+    Database db = await instance.database;
+    return await db
+        .delete(table, where: '$columnEmail = ?', whereArgs: [email]);
+  }
+}
--- lib/screens/profile.dart
+++ lib/screens/profile.dart
@@ -1,6 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:project/components/blackButtonRound.dart';
 import 'package:project/screens/history.dart';
+import 'package:project/components/database.dart';
 
 class profile extends StatefulWidget {
   const profile({super.key});
@@ -10,107 +13,273 @@ class profile extends StatefulWidget {
 }
 
 class _profileState extends State<profile> {
+  DatabaseHelper mydb = DatabaseHelper();
+  Map<String, dynamic> userData = {};
+  Map<String, dynamic> userDataCloud = {};
+  User? user = FirebaseAuth.instance.currentUser;
+  bool loading = true;
+  Future<void> fetchUserDataFromLocalDB(String email) async {
+    try {
+      // Query the database for the user with the specified email
+      List<Map<String, dynamic>> queryResult = await mydb.queryRows(email);
+      if (queryResult.isNotEmpty) {
+        // Assuming there's only one match for the given email
+        setState(() {
+          userData = queryResult.first;
+        });
+      } else {
+        print("No local user data found for email $email");
+      }
+    } catch (e) {
+      print('Error fetching user data from local DB: $e');
+    }
+    setState(() {
+      loading = false;
+    });
+  }
+
+  Future<void> saveUserDataToLocalDB(Map<String, dynamic> userData) async {
+    try {
+      // Map the userData to the database columns
+      Map<String, dynamic> row = {
+        DatabaseHelper.columnEmail: userData['email'],
+        DatabaseHelper.columnName: userData['name'],
+        DatabaseHelper.columnLevel: userData['level'],
+        DatabaseHelper.columnPhoneNumber: userData['phone'],
+        // Change 'phone' to the actual key in your userData map
+      };
+
+      // Insert the user data into the SQLite database
+      final id = await mydb.insert(row);
+      print('Inserted user data to local DB with id: $id');
+    } catch (e) {
+      print('Error saving user data to local DB: $e');
+    }
+  }
+
+  // Fetch a user document from Firestore by email
+  Future<void> fetchUserByEmail(String? email) async {
+    try {
+      // Reference to Firestore collection
+      var userCollection = FirebaseFirestore.instance.collection('users');
+
+      // Query for documents with the matching email
+      var querySnapshot =
+          await userCollection.where('email', isEqualTo: email).limit(1).get();
+
+      // Check if any documents were found
+      if (querySnapshot.docs.isNotEmpty) {
+        // Return the first document found (should be the only one due to the query)
+        setState(() {
+          print("querySnapshot.docs.first.data()");
+          print(querySnapshot.docs.first.data());
+          print("querySnapshot.docs.first.data()");
+
+          userDataCloud = querySnapshot.docs.first.data();
+        });
+        saveUserDataToLocalDB(userDataCloud);
+      } else {
+        print(user?.email);
+        print(email);
+        print("Non Found");
+        // No documents found
+      }
+    } catch (e) {
+      // Handle any errors here
+      print(e);
+    }
+    fetchUserDataFromLocalDB(user!.email!);
+  }
+
+  @override
+  void initState() {
+    fetchUserByEmail(user?.email);
+    super.initState();
+    // Fetch the user document by email
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: Text("Profile"),
-        backgroundColor: Colors.black,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            blackButtonRound(
-                text: "Trips History",
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => history(),
-                    ),
-                  );
-                }),
-            SizedBox(height: 80),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ClipOval(
-                    child: Image.asset(
-                      "lib/assets/user.png",
-                      height: 85,
-                      width: 85,
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  const Text(
-                    'Ziad Nasr',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8),
-                        child: Text(
-                          "Pinned Email",
-                          style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.grey,
-                              fontWeight: FontWeight.bold),
-                        ),
-                      ),
-                      TextField(
-                        enabled: false,
-                        decoration: InputDecoration(
-                            labelText: '19P****@eng.asu.edu.eg',
-                            border: OutlineInputBorder(),
-                            labelStyle: TextStyle(color: Colors.black)),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                            hintText: "01023456789",
-                            labelText: 'Phone Number',
-                            border: OutlineInputBorder(),
-                            labelStyle: TextStyle(color: Colors.black)),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      TextField(
-                        decoration: InputDecoration(
-                            hintText: "Senior",
-                            labelText: 'Academic Level',
-                            border: OutlineInputBorder(),
-                            labelStyle: TextStyle(color: Colors.black)),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 24),
-                  blackButtonRound(text: "Confirm Changes", onPressed: () {})
-                ],
-              ),
-            ),
-          ],
+    if (loading) {
+      return Center(child: CircularProgressIndicator());
+    } else {
+      return MaterialApp(
+          home: Scaffold(
+        appBar: AppBar(
+          title: Text("Profile"),
+          backgroundColor: Colors.black,
         ),
-      ),
-    ));
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              blackButtonRound(
+                  text: "Trips History",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => history(),
+                      ),
+                    );
+                  }),
+              SizedBox(height: 80),
+              Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ClipOval(
+                      child: Image.asset(
+                        "lib/assets/user.png",
+                        height: 85,
+                        width: 85,
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    Text(
+                      userData[DatabaseHelper.columnName] ?? 'No Name',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Padding(
+                      padding: EdgeInsets.all(5), // 5px margin on all sides
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Align the label to the left
+                        children: [
+                          const Text(
+                            'Pinned Email', // The label text
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey, // Label font size
+                              fontWeight:
+                                  FontWeight.bold, // Make the label bold
+                            ),
+                          ),
+                          const SizedBox(
+                              height:
+                                  5), // Space between the label and the container
+                          Container(
+                            width: MediaQuery.of(context).size.width -
+                                10, // Screen width minus the total horizontal padding
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 16),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.black), // Border color
+                              borderRadius: BorderRadius.circular(
+                                  4), // Rounded corner radius
+                            ),
+                            child: Text(
+                              userData[DatabaseHelper
+                                  .columnEmail], // Replace with the default text if user?.email is null
+                              style: TextStyle(
+                                color: Colors.black, // Text color
+                                fontSize: 16, // Font size
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 7),
+                    Padding(
+                      padding: EdgeInsets.all(5), // 5px margin on all sides
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Align the label to the left
+                        children: [
+                          const Text(
+                            'Phone Number', // The label text
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey, // Label font size
+                              fontWeight:
+                                  FontWeight.bold, // Make the label bold
+                            ),
+                          ),
+                          const SizedBox(
+                              height:
+                                  5), // Space between the label and the container
+                          Container(
+                            width: MediaQuery.of(context).size.width -
+                                10, // Screen width minus the total horizontal padding
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 16),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.black), // Border color
+                              borderRadius: BorderRadius.circular(
+                                  4), // Rounded corner radius
+                            ),
+                            child: Text(
+                              userData[DatabaseHelper
+                                  .columnName], // Replace with the default text if user?.email is null
+                              style: TextStyle(
+                                color: Colors.black, // Text color
+                                fontSize: 16, // Font size
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 7),
+                    Padding(
+                      padding: EdgeInsets.all(5), // 5px margin on all sides
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment
+                            .start, // Align the label to the left
+                        children: [
+                          const Text(
+                            'Academic Level', // The label text
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey, // Label font size
+                              fontWeight:
+                                  FontWeight.bold, // Make the label bold
+                            ),
+                          ),
+                          const SizedBox(
+                              height:
+                                  5), // Space between the label and the container
+                          Container(
+                            width: MediaQuery.of(context).size.width -
+                                10, // Screen width minus the total horizontal padding
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 8, vertical: 16),
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                  color: Colors.black), // Border color
+                              borderRadius: BorderRadius.circular(
+                                  4), // Rounded corner radius
+                            ),
+                            child: Text(
+                              userData[
+                                  "level"], // Replace with the default text if user?.email is null
+                              style: TextStyle(
+                                color: Colors.black, // Text color
+                                fontSize: 16, // Font size
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 24),
+                    blackButtonRound(text: "Update Profile", onPressed: () {})
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ));
+    }
   }
 }
